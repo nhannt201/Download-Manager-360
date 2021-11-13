1298c448142ce63eab0f5ae5f83c6f940db8e74f
Nguyễn Trung Nhẫn <trungnhan21.12@gmail.com>
2021-11-13 09:08:28 +0700

    Full Source đi kèm

---
+++ Download Manager 360 Install/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.pro = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.urll = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(170, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(186, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cài đặt Download Manager 360"
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(533, 277)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "&Cài đặt"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(391, 277)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(120, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Chọn đường dẫn"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Download Manager 360", "Phần mềm tải Tập Tin do nhóm nghiên cứu Lưu trữ 360 phát hành với giao diện gọn g" & _
+                "àng dễ sử dụng ", "", "Hướng dẫn cài đặt :", "- Click chọn đường dẫn ", "- Sau đó chọn tới thư mục hay ổ đĩa mà bạn muốn cài đặt ", "- Sau đó nhấn Cài đặt", "Quá trình cái đặt :", "- Chương trình sẽ tạo ra thư mục Download Manager 360 ", "- Sau đó sao đó kết nối đến máy chủ để cài đặt các tập tin mới nhất", "- Cuối cùng cài đặt và sao chép  xong chương trình sẽ tự động thoát và mở Downloa" & _
+                "d Manager 360"})
+        Me.ListBox1.Location = New System.Drawing.Point(12, 81)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(596, 186)
+        Me.ListBox1.TabIndex = 6
+        '
+        'pro
+        '
+        Me.pro.Location = New System.Drawing.Point(12, 278)
+        Me.pro.Name = "pro"
+        Me.pro.Size = New System.Drawing.Size(373, 22)
+        Me.pro.TabIndex = 7
+        Me.pro.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 2000
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = Global.Download_Manager_360_Install.My.Resources.Resources._3
+        Me.PictureBox1.Location = New System.Drawing.Point(49, 25)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(141, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Download Manager 360"
+        '
+        'urll
+        '
+        Me.urll.AutoSize = True
+        Me.urll.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.urll.Location = New System.Drawing.Point(170, 65)
+        Me.urll.Name = "urll"
+        Me.urll.Size = New System.Drawing.Size(0, 13)
+        Me.urll.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(636, 315)
+        Me.Controls.Add(Me.urll)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.pro)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Download Manager 360 Install"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents pro As System.Windows.Forms.ProgressBar
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents urll As System.Windows.Forms.Label
+
+End Class
---
+++ Download Manager 360 Install/Form1.vb
@@ -0,0 +1,199 @@
+﻿Imports System.Net
+Imports System.IO
+Imports IWshRuntimeLibrary
+Imports System.Environment
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Dim a As String
+    Dim InstallRev As String
+    Dim InstallTo As String
+
+
+
+
+    '
+   
+
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If FolderBrowserDialog1.ShowDialog = Windows.Forms.DialogResult.Cancel = True Then
+            MsgBox("Bạn chưa chọn đường dẫn !")
+        Else
+            a = FolderBrowserDialog1.SelectedPath
+            urll.Text = FolderBrowserDialog1.SelectedPath
+            Button1.Enabled = True
+        End If
+    End Sub
+    Public Function FExists(OrigFile As String)
+        Dim fs
+        fs = CreateObject("Scripting.FileSystemObject")
+        FExists = fs.fileexists(OrigFile)
+    End Function
+    'Returns a boolean - True if the file exists
+
+
+    Public Function DirExists(OrigFile As String)
+        Dim fs
+        fs = CreateObject("Scripting.FileSystemObject")
+        DirExists = fs.folderexists(OrigFile)
+    End Function
+    'Returns a boolean - True if the folder exists
+
+
+    Public Function DExists(OrigFile As String)
+        Dim fs, d
+        fs = CreateObject("Scripting.FileSystemObject")
+        If fs.driveexists(OrigFile) = True Then
+            d = fs.getdrive(OrigFile)
+            DExists = 1
+            If d.isready = True Then
+                DExists = 2
+                Exit Function
+            End If
+        Else
+            DExists = 0
+        End If
+    End Function
+
+
+       Private Sub CreateShortCut(ByVal FileName As String, ByVal Title As String)
+        Try
+            Dim WshShell As New WshShell
+            ' short cut files have a .lnk extension
+            Dim shortCut As IWshRuntimeLibrary.IWshShortcut = DirectCast(WshShell.CreateShortcut(FileName), IWshRuntimeLibrary.IWshShortcut)
+
+            ' set the shortcut properties
+            With shortCut
+                .TargetPath = urll.Text & "\Download Manager 360\Download Manager 360.exe"
+                .WindowStyle = 1I
+                .Description = "Download Manager 360"
+                .WorkingDirectory = urll.Text
+                ' the next line gets the first Icon from the executing program
+                .IconLocation = Application.ExecutablePath & ", 0"
+                .Arguments = String.Empty
+                .Save() ' save the shortcut file
+            End With
+        Catch ex As System.Exception
+
+        End Try
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        InstallTo = urll.Text
+
+    End Sub
+    Private Sub client_DownloadCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.AsyncCompletedEventArgs)
+        Try
+
+            Label1.Text = "Đang tạo khóa..."
+            ' Timer1.Enabled = True
+            '  UnZip(urll.Text & "\Download Manager 360\setup.zip", urll.Text & "\Download Manager 360")
+            'If FExists(a & "\Download Manager 360\setup.zip") = True Then
+            'Kill(a & "\Download Manager 360\setup.zip")
+            '  Else
+
+            ' End If
+            'My.Computer.FileSystem.CopyDirectory(Application.StartupPath & "\Download Manager 360", urll.Text & "\Download Manager 360", True)
+            Process.Start(a & "\Download Manager 360\Download Manager 360.exe")
+            CreateShortCut(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Download Manager 360.lnk"), "Download Manager 360")
+            ' InstallToSYSaRUN()
+            End
+        Catch
+            MsgBox("Có lỗi xảy ra , tạo khóa thất bại!")
+            Me.Close()
+        End Try
+    End Sub
+
+    Sub InstallToSYSaRUN()
+
+     
+        Try
+
+            My.Computer.Registry.LocalMachine.CreateSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Download Manager 360")
+            My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Download Manager 360", "DisplayName", "Download Manager 360")
+            My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Download Manager 360", "DisplayIcon", InstallTo + "\Download Manager 360.exe,0")
+            My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Download Manager 360", "DisplayVersion", InstallRev)
+            My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Download Manager 360", "HelpLink", "mailto:trungnhan21.12@gmail.com")
+            My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Download Manager 360", "Publisher", "Download Manager 360 - Nguyễn Trung Nhẫn")
+            My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Download Manager 360", "UninstallString", InstallTo + "\Uninstall.exe")
+            My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Download Manager 360", "URLInfoAbout", "http://download-manager-360.luutru360.com/")
+            '  MsgBox("Cài đặt thành công!")
+            'Shell(InstallTo & "\Download Manager 360.exe")
+            'Me.Close()
+        Catch ex As Exception
+
+            Me.Close()
+        End Try
+    End Sub
+
+
+    Private Sub client_ProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs)
+        Dim bytesIn As Double = Double.Parse(e.BytesReceived.ToString())
+        Dim totalBytes As Double = Double.Parse(e.TotalBytesToReceive.ToString())
+        Dim percentage As Double = bytesIn / totalBytes * 100
+
+        pro.Value = Int32.Parse(Math.Truncate(percentage).ToString())
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim fd As String = DirExists(a & "\Download Manager 360")
+
+
+            If fd = True Then
+
+                MessageBox.Show("Thư mục Download Manager 360 đã tồn tại !")
+
+            ElseIf fd = False Then
+                Label1.Text = "Đang cài đặt ... Vui lòng đợi trong giây lác..."
+                pro.Visible = True
+                Button2.Enabled = False
+                Button1.Enabled = False
+                If Not Directory.Exists(a & "\Download Manager 360") Then
+                    Directory.CreateDirectory(a & "\Download Manager 360")
+                    '  Directory.CreateDirectory(Application.StartupPath & "\Download Manager 360")
+                    Dim client As WebClient = New WebClient
+                    Dim client2 As WebClient = New WebClient
+                    Dim client3 As WebClient = New WebClient
+                    Dim client4 As WebClient = New WebClient
+                    AddHandler client.DownloadProgressChanged, AddressOf client_ProgressChanged
+                    AddHandler client.DownloadFileCompleted, AddressOf client_DownloadCompleted
+                    '  client.DownloadFileAsync(New Uri("http://download-manager-360.luutru360.com/install/install/zip"), Application.StartupPath & "\Download Manager 360\setup.zip")
+                    client.DownloadFileAsync(New Uri("http://download-manager-360.luutru360.com/install/Download Manager 360.exe"), urll.Text & "\Download Manager 360\Download Manager 360.exe")
+                    client2.DownloadFileAsync(New Uri("http://download-manager-360.luutru360.com/install/Download Manager 360.pdb"), urll.Text & "\Download Manager 360\Download Manager 360.pdb")
+                    client3.DownloadFileAsync(New Uri("http://download-manager-360.luutru360.com/install/Download Manager 360.vshost.exe"), urll.Text & "\Download Manager 360\Download Manager 360.vshost.exe")
+                    client4.DownloadFileAsync(New Uri("http://download-manager-360.luutru360.com/install/Download Manager 360.vshost.exe.manifest"), urll.Text & "\Download Manager 360\Download Manager 360.vshost.exe.manifest")
+                End If
+            End If
+        Catch
+            MsgBox("Có lỗi xảy ra , không thể cài đặt!")
+            End
+        End Try
+    End Sub
+
+    '   Dim shObj As Object = Activator.CreateInstance(Type.GetTypeFromProgID("Shell.Application"))
+    ' Sub UnZip(zipfile As String, outfolder As String)
+
+    'Dim output As Object = shObj.NameSpace((outfolder))
+    ' Dim input As Object = shObj.NameSpace((zipfile))
+    '      output.CopyHere((input.Items), 4)
+    '  End Sub
+    Private Sub pro_Click(sender As Object, e As EventArgs) Handles pro.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Process.Start(a & "\Download Manager 360\Download Manager 360.exe")
+        End
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
